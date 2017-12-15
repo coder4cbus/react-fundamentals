a78e24182689cfed5f9b1dea7ec2978a45f9be87
Endava Computer <estebanmarin002@gmail.com>
2017-12-15 14:01:13 -0500

    last commit from the office

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,35 +1,39 @@
 import React from "react";
 import api from "../utils/api";
-import queryString from 'query-string';
-
+import queryString from "query-string";
 
 export default class Results extends React.Component {
-    constructor(props){
-        super(props);
-        this.state= {
-            error:'',
-            winner:[],
-            loser: [],
-            loading:true,
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      winner: null,
+      loser: null,
+      loading: true
+    };
+  }
 
-    componentDidMount(){
-        const player = queryString.parse(this.props.location.search);
-        api.battle([
-            player.playerOneName,
-            player.playerTwoName
-            ])
-            .then(results =>{
-            console.log(results)
-        });
-    }
+  componentDidMount() {
+    const player = queryString.parse(this.props.location.search);
+    api.battle([player.playerOneName, player.playerTwoName]).then(results => {
+      console.log(results);
+      this.setState = results => {
+        return {
+          error: "",
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        };
+      };
+    });
+  }
 
   render() {
+    //   const [error, winner, loser, loading] = this.state;
     return (
       <div>
-        {this.state.loading && 'Loading!!'}
-
+        {this.state.loading && "Loading!!"}
+        {/* {console.log(loading)} */}
       </div>
     );
   }
