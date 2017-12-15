80c045dafeba650f71346f65d5fd307a5ff3cb33
Endava Computer <estebanmarin002@gmail.com>
2017-12-15 09:35:23 -0500

    Praparing the Axios Api

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { getProfile } from "../utils/api";
+import api from "../utils/api";
+import queryString from 'query-string';
+
 
 export default class Results extends React.Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            error:'',
+            winner:[],
+            loser: [],
+            loading:true,
+        }
+    }
+
+    componentDidMount(){
+        const player = queryString.parse(this.props.location.search);
+        api.battle([
+            player.playerOneName,
+            player.playerTwoName
+            ])
+            .then(results =>{
+            console.log(results)
+        });
+    }
+
   render() {
     return (
       <div>
-        Results!!
-        {console.log(getProfile)}
+        {this.state.loading && 'Loading!!'}
+
       </div>
     );
   }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,47 @@
 import axios from "axios";
 
+const getProfile = (username)=>{
+  return (axios.get(`https://api.github.com/users/${username}`)
+    .then(user=> user.data))
+}
+
+const getRepos = (username)=>{
+  return axios.get(`https://api.github.com/users/${username}/repos`)
+}
+
+const getStarCount = (repos)=>{
+  return repos.data.reduce((count,repo)=>{
+    return count + repo.stargazers_count;
+  },0);
+}
+
+const calculateScore = (profile,repos)=>{
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+  return (followers*3)+totalStars;
+}
+
+function handleError(){
+  console.warn(error);
+  return null;
+}
+
+const getUserData = (player)=>{
+  return axios.all(
+    getProfile(player),
+    getRepos(player)
+  ).then((data)=>{
+    return {
+      profile: data[0],
+      score: data[1]
+    }
+  });
+}
+
 module.exports = {
   battle: username => {
-    return axios.get(`https://api.github.com/users/${username}`);
+    console.log(username)
+    return getProfile(username[0]);
   },
   fetchPopularRepos: language => {
     const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:
